95f2f67c0a21e8dace046b2994618bf93e5bff83
mohmedeprahem <m.eprahem.168@gmail.com>
2024-02-23 18:08:54 +0200

    refactor: Add range values in 'Price' and 'Quantity'

--- src/main/DTOs/CreateItemRequestDto.cs
+++ src/main/DTOs/CreateItemRequestDto.cs
@@ -9,9 +9,11 @@ namespace main.DTOs
         public string Title { get; set; }
 
         [Required(ErrorMessage = "Please enter the price")]
+        [Range(0, double.MaxValue, ErrorMessage = "Price must be a positive number")]
         public double Price { get; set; }
 
         [Required(ErrorMessage = "Please enter the Quantity")]
+        [Range(1, int.MaxValue, ErrorMessage = "Quantity must be at least 1")]
         public int Quantity { get; set; }
     }
 }
