c4186a3fc22b795edea98865c30ba914c236998a
mohmedeprahem <m.eprahem.168@gmail.com>
2024-02-23 18:23:47 +0200

    feat: Add Create Action method as GET and POST.

---
+++ src/main/Controllers/ItemController.cs
@@ -0,0 +1,39 @@
+﻿using main.DTOs;
+using main.Interfaces.Services;
+using main.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace main.Controllers
+{
+    [Route("items")]
+    public class ItemController : Controller
+    {
+        private readonly IItemService itemService;
+
+        public ItemController(IItemService itemService)
+        {
+            this.itemService = itemService;
+        }
+
+        [HttpGet]
+        [Route("create")]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("create")]
+        public async Task<ActionResult> Create(CreateItemRequestDto itemDto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            Item item = await itemService.CreateItemAsync(itemDto);
+
+            return Created();
+        }
+    }
+}
