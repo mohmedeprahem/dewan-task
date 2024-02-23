a488a4ac245db8d17f5d7e9437bda276a0ca2217
mohmedeprahem <m.eprahem.168@gmail.com>
2024-02-23 18:18:39 +0200

    feat: Add 'CreateItemAsync' method with item service class and interface.

---
+++ src/main/Interfaces/Services/IItemService.cs
@@ -0,0 +1,10 @@
+﻿using main.DTOs;
+using main.Models;
+
+namespace main.Interfaces.Services
+{
+    public interface IItemService
+    {
+        Task<Item> CreateItemAsync(CreateItemRequestDto createItemDto);
+    }
+}
---
+++ src/main/Services/ItemService.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using main.DTOs;
+using main.Interfaces.Repository;
+using main.Interfaces.Services;
+using main.Models;
+
+namespace main.Services
+{
+    public class ItemService : IItemService
+    {
+        private readonly IItemRepository _itemRepository;
+        private readonly IMapper mapper;
+
+        public ItemService(IItemRepository itemRepository, IMapper mapper)
+        {
+            _itemRepository = itemRepository;
+            this.mapper = mapper;
+        }
+
+        public Task<Item> CreateItemAsync(CreateItemRequestDto createItemDto)
+        {
+            Item item = mapper.Map<Item>(createItemDto);
+
+            return _itemRepository.CreateItemAsync(item);
+        }
+    }
+}
