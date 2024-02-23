b89ed15610f6a44de36f3995e5e421bdbc5411c1
mohmedeprahem <m.eprahem.168@gmail.com>
2024-02-23 14:49:31 +0200

    feat: Add item and receipt model files.

---
+++ src/main/Models/Item.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace main.Models
+{
+    public class Item
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Title { get; set; }
+
+        [Required]
+        public double Price { get; set; }
+
+        [Required]
+        public int Quantity { get; set; }
+
+        public ICollection<Receipt> Receipts { get; set; }
+    }
+}
---
+++ src/main/Models/Receipt.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace main.Models
+{
+    public class Receipt
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public double TotalPrice { get; set; }
+
+        [Required]
+        public double PaidPrice { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        public ICollection<Item> Items { get; set; }
+    }
+}
