29edb77564d4bc1ddb79e4ee099e5019c22d1d05
mohmedeprahem <m.eprahem.168@gmail.com>
2024-02-24 20:32:15 +0200

    feat: Add ItemRreceipt model.

---
+++ src/main/Models/ItemReceipt.cs
@@ -0,0 +1,11 @@
+﻿namespace main.Models
+{
+    public class ItemReceipt
+    {
+        public int ItemId { get; set; }
+        public Item Item { get; set; }
+
+        public int ReceiptId { get; set; }
+        public Receipt Receipt { get; set; }
+    }
+}
