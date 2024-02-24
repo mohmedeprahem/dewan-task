8d452d5fb8f8255353da8ffb547acc3a296f1dc1
mohmedeprahem <m.eprahem.168@gmail.com>
2024-02-24 21:18:44 +0200

    feat: Add ReceiptResponseDto class.

---
+++ src/main/DTOs/ReceiptResponseDto.cs
@@ -0,0 +1,10 @@
+﻿namespace main.DTOs
+{
+    public class ReceiptResponseDto
+    {
+        public int Id { get; set; }
+        public double TotalPrice { get; set; }
+        public double PaidAmount { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
